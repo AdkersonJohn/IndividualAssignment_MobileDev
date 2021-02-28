0733c29b5e513e1d14518e4c3e7c164c8decf7f3
AdkersonJohn <61293447+AdkersonJohn@users.noreply.github.com>
2021-02-28 17:01:16 -0500

    Attach our Live Data to our Autocomplete

--- app/src/main/java/app/plantdiary/individualassignment3048q/MainActivity.kt
+++ app/src/main/java/app/plantdiary/individualassignment3048q/MainActivity.kt
@@ -3,8 +3,10 @@ package app.plantdiary.individualassignment3048q
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import app.plantdiary.individualassignment3048q.ui.main.MainFragment
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
+    var userInput = ""
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
